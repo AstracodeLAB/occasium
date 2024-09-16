4bb1a7bf4107c63f6bc66125f1302ae678e38233
AstracodeLAB <astracodelab@gmail.com>
2024-09-16 17:28:52 +0200

    Add footer nav menu

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
 	content: ['./src/**/*.{js,ts,jsx,tsx,astro}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				accent: 'rgb(136, 58, 234)', // Color del header
+				'accent-light': 'rgb(224, 204, 250)',
+				'accent-dark': 'rgb(49, 10, 101)',
+				'background': '#f5f5f5', // Color de fondo claro
+				'text-color': '#333', // Color de texto oscuro
+				'button-bg': 'rgb(136, 58, 234)', // Color de fondo del botón
+				'button-hover-bg': 'rgb(49, 10, 101)', // Color de fondo del botón al pasar el cursor
+			},
+		},
 	},
 	plugins: [],
 };
